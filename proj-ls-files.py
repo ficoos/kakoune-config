9082abd562f9f004eca4a83d84be3b4383e4dcf6
Dima Kuznetsov <dmitrykuzn@gmail.com>
2019-03-04 22:06:30 +0200

    Use git find even when not in project root

--- proj-ls-files.py
+++ proj-ls-files.py
@@ -34,7 +34,7 @@ def get_git_files():
 def get_find_files():
     get_files(['find', '-type', 'f', '-follow'], 'find')
 
-if os.path.exists('.git'):
+if subprocess.call(('git', 'rev-parse')) == 0:
     get_git_files()
 else:
     get_find_files()
