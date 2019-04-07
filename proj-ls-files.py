19c8c5784deee4531e5939ed06c7c8d236db747f
Saggi Mizrahi <saggi@mizrahi.cc>
2019-04-07 16:59:04 +0300

    fix pep8 comments in proj-ls-files.py

--- proj-ls-files.py
+++ proj-ls-files.py
@@ -4,6 +4,7 @@ import os
 
 LINE_FORMAT = '\033[33m{prefix} \033[37m{path}\033[0m{base}'
 
+
 def get_files(cmd, prefix):
     p = subprocess.Popen(
         cmd,
@@ -22,17 +23,29 @@ def get_files(cmd, prefix):
     finally:
         p.wait()
 
+
 def get_git_files():
     # TODO: do this in the git root
     get_files(['git', 'ls-files', '-c'], 'git')
     get_files(['git', 'ls-files', '-o', '--exclude-standard'], 'git-other')
     if os.path.exists('.gitmodules'):
-        mod_prefixes = subprocess.check_output(['git', 'config', '--file', '.gitmodules', '--get-regexp', 'path'])
+        mod_prefixes = subprocess.check_output([
+            'git',
+            'config',
+            '--file',
+            '.gitmodules',
+            '--get-regexp',
+            'path'])
         mod_prefixes = [l.decode('utf8') for l in mod_prefixes.split()]
-        get_files(['git', 'ls-files', '--recurse-submodules'] + mod_prefixes, 'git-submodules')
+        get_files(['git',
+                   'ls-files',
+                   '--recurse-submodules'] + mod_prefixes,
+                  'git-submodules')
+
 
 def get_find_files():
     get_files(['find', '-type', 'f', '-follow'], 'find')
+
 
 if subprocess.call(('git', 'rev-parse'), stderr=subprocess.DEVNULL) == 0:
     get_git_files()
