7fa3bab5e5af3e994cab726d1a3f530b4c492c78
Saggi Mizrahi <saggi@mizrahi.cc>
2020-04-21 16:59:09 +0300

    [fzf] pop at top of window instead of bottom of pane
    This will ensure that even if pane is very small fzf is shown properly

--- tmux-fzf.sh
+++ tmux-fzf.sh
@@ -5,7 +5,7 @@ status_fifo=$(mktemp -u)
 mkfifo $status_fifo
 exec 3<>$status_fifo
 rm -f $status_fifo
-tmux split-pane -p 30 -- sh -c "fzf ${args} >/proc/$ppid/fd/1 </proc/$ppid/fd/0; echo \$? >>/proc/$ppid/fd/3"
+tmux split-pane -f -b -p 30 -- sh -c "fzf ${args} >/proc/$ppid/fd/1 </proc/$ppid/fd/0; echo \$? >>/proc/$ppid/fd/3"
 sleep 0.2
 read l <&3
 exit $l
